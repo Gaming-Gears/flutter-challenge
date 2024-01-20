1dacae821b84cdcdddf5f89c6abf4a143368aaab
XxThunderBlastxX <koustavmondal55@gmail.com>
2024-01-20 18:43:36 +0530

    Modified setting model

--- lib/src/components/setting_button.dart
+++ lib/src/components/setting_button.dart
@@ -6,14 +6,75 @@ class SettingsButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: IconButton(
-        icon: Icon(Icons.settings),
-        onPressed: () => NesDialog.show(
-          context: context,
-          builder: (_) => Container(
-            child: Text('Settings'),
-          ),
+    return IconButton(
+      icon: const Icon(Icons.settings),
+      onPressed: () => NesDialog.show(
+        context: context,
+        builder: (_) => Column(
+          children: [
+            const Text('Settings'),
+            const SizedBox(height: 12),
+            NesButton(
+              type: NesButtonType.normal,
+              child: const Text('Resume'),
+              onPressed: () => Navigator.pop(context),
+            ),
+            const SizedBox(height: 12),
+            NesButton(
+              type: NesButtonType.normal,
+              child: const Text('Exit Game'),
+              onPressed: () =>
+                  Navigator.popUntil(context, (route) => route.isFirst),
+            ),
+            const SizedBox(height: 25),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Music'),
+                const SizedBox(width: 20),
+                Row(
+                  children: [
+                    NesButton(
+                      type: NesButtonType.success,
+                      child: const Text('On'),
+                      onPressed: () {},
+                    ),
+                    const SizedBox(width: 8),
+                    NesButton(
+                      type: NesButtonType.normal,
+                      child: const Text('Off'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Sound FX'),
+                const SizedBox(width: 20),
+                Row(
+                  children: [
+                    NesButton(
+                      type: NesButtonType.success,
+                      child: const Text('On'),
+                      onPressed: () {},
+                    ),
+                    const SizedBox(width: 8),
+                    NesButton(
+                      type: NesButtonType.normal,
+                      child: const Text('Off'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
