ba8f6e3ff35b36b9c3ffb95866891f194ad47beb
John Siegel <jpsiegel8@gmail.com>
2024-01-23 18:11:08 -0500

    Supporting up to 8k monitors

--- lib/src/game/background.dart
+++ lib/src/game/background.dart
@@ -7,7 +7,7 @@ import 'tiles/coordinates.dart';
 import 'tiles/tile.dart';
 
 final class Background extends SpriteComponent {
-  static const unitSize = 200;
+  static const unitSize = 325;
   static const cameraOffset = (-unitSize ~/ 2) * Tile.pixelSize;
 
   static Vector2 _offset(UnitCoordinates cameraCoordinates) => Vector2(
